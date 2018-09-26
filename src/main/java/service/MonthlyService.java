9557b54d957d42b56a30ff2806220c753453d537
Chen Li <lc_c8@hotmail.com>
2018-09-26 20:42:49 +0100

    try to implement observer pattern to budget controller and monthly yearly services

--- src/main/java/service/MonthlyService.java
+++ src/main/java/service/MonthlyService.java
@@ -2,11 +2,15 @@ package service;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
+import categories.Category;
 import expense.Expense;
 
 public interface MonthlyService {
 	BigDecimal addAnExpense(String name, BigDecimal amount, Date expenseDate);
 	void removeAnExpense(String name);
 	void addExpenseToCategory(Expense expense, String categoryName);
+	void createAnExpense(String name, BigDecimal amount, Date date);
+	void updateCategory(List<Category> categories);
 }
